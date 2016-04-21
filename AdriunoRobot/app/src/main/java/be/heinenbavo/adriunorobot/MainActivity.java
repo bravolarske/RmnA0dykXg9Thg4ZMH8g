73450f1cc35d5e00ac32f53e3672a74f8e7a2d21
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-04-21 14:38:45 +0200

    app updates :bug:

--- AdriunoRobot/app/src/main/java/be/heinenbavo/adriunorobot/MainActivity.java
+++ AdriunoRobot/app/src/main/java/be/heinenbavo/adriunorobot/MainActivity.java
@@ -1,6 +1,13 @@
 package be.heinenbavo.adriunorobot;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOError;
+import java.io.IOException;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -11,17 +18,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnStop;
+    private Button btnSend;
+    private EditText edtText;
 
     private static final int DISCOVER_DURATION = 300;
     private static final int REQUEST_BLU = 1;
@@ -33,12 +49,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        btnStop = (Button) findViewById(R.id.btnStop);
+        btnSend = (Button) findViewById(R.id.btnSend);
+        edtText = (EditText) findViewById(R.id.edtText);
 
-        btnStop.setOnClickListener(new View.OnClickListener() {
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                conectToBluetooth();
+                sendViaBluetooth();
             }
         });
 
@@ -52,21 +69,52 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void conectToBluetooth() {
+    private void sendViaBluetooth() {
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (mBluetoothAdapter == null) {
-            return;
+            Toast.makeText(this, "Bluetooth is not supported.", Toast.LENGTH_LONG);
         }
+        else {
+            //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
 
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //enableBtIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
 
-            enableBtIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
+            Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
 
-            startActivityForResult(enableBtIntent, REQUEST_BLU);
+            if (devices.size() != 0) {
 
+                for (BluetoothDevice device : devices){
+
+                    if (device.getName().equals("HC-06")){
+                        sendDataToPairedDevice(edtText.getText().toString(), device);
+                        break;
+                    }
+                }
+            }
+            //startActivityForResult(enableBtIntent, REQUEST_BLU);
+        }
+    }
+
+    private void sendDataToPairedDevice(String message ,BluetoothDevice device) {
+
+        byte[] toSend = message.getBytes();
+        try {
+            TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+            String uuid = tManager.getDeviceId();
+
+            UUID applicationUUID = UUID.fromString(uuid);
+            BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(applicationUUID);
+            OutputStream mmOutStream = socket.getOutputStream();
+            mmOutStream.write(toSend);
+            // Your Data is sent to  BT connected paired device ENJOY.
+        } catch (IOException e) {
+            Log.e("", "Exception during write", e);
+        }
+        catch (Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("", ex.toString());
         }
     }
 
@@ -79,8 +127,8 @@ public class MainActivity extends AppCompatActivity {
             intent.setAction(Intent.ACTION_SEND);
             intent.setType("text/plain");
 
-            File file = new File(Environment.getExternalStorageDirectory(), "code.txt");
-            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            File file = new File(Environment.getExternalStorageDirectory(), "plop.txt");
+            intent.putExtra(Intent.EXTRA_STREAM, edtText.getText().toString());
 
             PackageManager pm = getPackageManager();
             List<ResolveInfo> list = pm.queryIntentActivities(intent, 0);
@@ -94,12 +142,30 @@ public class MainActivity extends AppCompatActivity {
                     packagename = info.activityInfo.packageName;
 
                     if (packagename.equals("com.android.bluetooth")){
-                        // https://www.youtube.com/watch?v=6hQ87u9v7SY (3:44)
+
+                        classname = info.activityInfo.name;
+
+                        found = true;
+                        break;
                     }
                 }
+
+                if (!found) {
+                    Toast.makeText(this, "Bluetooth haven't been found.", Toast.LENGTH_LONG);
+                }
+                else {
+                    //sendDataToPairedDevice(edtText.getText().toString() ,bluetoothDevice);
+                    intent.setClassName(packagename, classname);
+                    startActivity(intent);
+                }
+            }
+            else {
+                Toast.makeText(this, "Bleutooth is canceled", Toast.LENGTH_LONG);
             }
         }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
